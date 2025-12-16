48b6cc21622f813f8963f4588debb6866d8eb542
th-thong <thongtran.dev@gmail.com>
2025-12-16 23:08:35 +0700

    ci(frontend): add Dockerfile for frontend

--- src/frontend/next.config.mjs
+++ src/frontend/next.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://127.0.0.1:8000/api/:path*',
+        destination: `${process.env.API_URL || 'http://127.0.0.1:8000'}/api/:path*`,
       },
     ]
   },
 }
 
-export default nextConfig
+export default nextConfig;
